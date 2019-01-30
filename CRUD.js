b7228ef63f89befe16a33b9c60ec3932ebc7e375
DESKTOP-OGFNBNT\ASUS <sergiycheban1499@gmail.com>
2019-01-31 01:25:38 +0200

    add comments

--- CRUD.js
+++ CRUD.js
@@ -1,10 +1,14 @@
 var CRUD = {
+  // Изтрива събитие по уникален идентификатор, и извежда съобщение за успешно
+  // извършена операция
   deleteEvent: function(id) {
     var index = arrayOfEvents.findIndex(x => x.id == id);
     arrayOfEvents.splice(index, 1);
     localStorage.setItem("arrayOfEvents", JSON.stringify(arrayOfEvents));
     document.location.reload(true);
   },
+
+  // Премахнете присъстващ потребител от събитието.
   deleteVisitor: function(index, name) {
     var i = arrayOfEvents[index].people.findIndex(x => x.name == name);
     arrayOfEvents[index].people.splice(i, 1);
@@ -12,6 +16,11 @@ var CRUD = {
     document.location.reload(true);
   },
 
+  // Създава ново събитие. Задължителни атрибути на събитието са неговото име. Ако
+  // потребителя не подаде флаг, указващ дали събитието е подходящо за непълнолетни то по
+  // подразбиране е.
+  // Актуализира събитие по уникален идентификатор и изведете съобщение за правилно
+  // извършена операция.
   newElement: function() {
     var d1 = document.getElementById("listAdmin");
     var inputValueTitle = document.getElementById("title").value;
@@ -25,23 +34,24 @@ var CRUD = {
     var id = Utils.getUniqueId();
     console.log(inputValueTitle);
 
-    if (
-      inputValueTitle === "" ||
-      inputValueContent === "" ||
-      inputValueTitle === ""
-    ) {
+    if (inputValueTitle === "" || inputValueContent === "") {
       alert("You must write something!");
     } else {
       arrayOfEvents.push({
         id: id,
         title: inputValueTitle,
         content: inputValueContent,
+        // Добавете възможност за запазване на текуща дата на събитието.
         dateTime: inputValueDate,
         isAdulthood: inputValueCheckbox,
+        // Добавете, свойство цена към всяко събитие което организирате.
         price: inputPrice,
         isArchive: false,
         people: []
       });
+
+      alert("Event added");
+
       d1.insertAdjacentHTML(
         "afterend",
         '<div class="card"> <a class="level-right" onClick="CRUD.deleteEvent(this.id)" id="' +
---
+++ generateHtmlList.js
@@ -0,0 +1,93 @@
+// Извежда всички вече съхранени събития, като визуализира цялата необходима
+// информация за тях.
+function generateList(events) {
+  events.map(function(obj) {
+    listNumberOfVisitors.push(obj.people.length);
+    var list = document.getElementById("listAdmin");
+
+    var visibility18 = obj.isAdulthood ? "" : "is-hidden";
+    var visibilityPrice = obj.price != 0 ? "" : "is-hidden";
+
+    list.insertAdjacentHTML(
+      "afterend",
+      '<div class="card has-background-light"> <a class="level-right" onClick="CRUD.deleteEvent( this.id )" id="' +
+        obj.id +
+        '"class="button is-danger is-outlined"> <span class="icon is-small"> <i class="fas fa-times"></i> </span> </a> <div class="card-content"> <div class="media"> <div class="media-content"> <p class="title is-4">' +
+        obj.title +
+        '</div> </div> <div class="content">' +
+        obj.content +
+        "<br /> <time>" +
+        obj.dateTime +
+        '</time> </div> </div><div class="level-right ' +
+        visibilityPrice +
+        '"> <figure class="image is-24x24"> <img src="resources/price-icon-22.png"> </div> <div class="level-right ' +
+        visibility18 +
+        '"> <figure class="image is-24x24"> <img src="resources/18.png"> </div> <footer class="card-footer">' +
+        '<p class="card-footer-item">' +
+        obj.people.length +
+        " people will go to this event</p>" +
+        '<a id="' +
+        obj.id +
+        '" onClick="showVisitors(this.id,\'' +
+        "Female" +
+        '\')" class="card-footer-item">Women visitors</a>' +
+        '<a id="' +
+        obj.id +
+        '" onClick="showVisitors(this.id,\'' +
+        "Male" +
+        '\')" class="card-footer-item">Man visitors</a>' +
+        '<a id="' +
+        obj.id +
+        '" onClick="showVisitors(this.id)" class="card-footer-item">All visitors</a>' +
+        " </footer></div>"
+    );
+  });
+}
+
+function generateListForUser(idList = "listForUser") {
+  arrayOfEvents.map(function(obj) {
+    var d = document.getElementById(idList);
+
+    var visibility18 = obj.isAdulthood ? "" : "is-hidden";
+    var visibilityPrice = obj.price != 0 ? "" : "is-hidden";
+
+    d.insertAdjacentHTML(
+      "afterend",
+      ' <div class="column is-4"> <div class="card"> <div class="card-image">' +
+        '<figure class="image is-4by3"> <img src="resources/photo-1520011597487-ebdd1ea20ab0.jpg" alt="Placeholder image" class="modal-button" data-target="modal-image2">' +
+        '</img> </figure> </div> <div class="card-content"> <div class="content"> <h4>' +
+        obj.title +
+        ": " +
+        obj.dateTime +
+        "</h4> <p>" +
+        obj.content +
+        '</p>     <div class="columns"> <div class="column">  <button class="button is-warning" id="' +
+        obj.id +
+        '"  onClick="addClass(true , this.id)">I WILL GO</button> </div><div class="column"> </div> <div class="level-right ' +
+        visibilityPrice +
+        '"><p class="has-text-danger"> $ </p></div><div class="column"> </div> <div class="level-right ' +
+        visibility18 +
+        '"><p class="has-text-danger"> 18+ </p></div> </div> </div>  </div>  </div> </div>'
+    );
+  });
+}
+
+function generateListVisitor(event, index) {
+  event.map(function(obj) {
+    var listUser = document.getElementById("listUser");
+    listUser.insertAdjacentHTML(
+      "afterend",
+      '<div class="card"> <a class="level-right" onClick="CRUD.deleteVisitor( ' +
+        index +
+        " ) " +
+        '"button is-danger is-outlined"> <span class="icon is-small"> <i class="fas fa-times"></i> </span> </a> <div class="card-content"> <div class="media"> <div class="media-content"> <p class="title is-4">' +
+        obj.name +
+        '</div> </div> <div class="content">' +
+        obj.gender +
+        "<br /> <time>" +
+        obj.age +
+        " yers" +
+        "</time> </div> </div></div>"
+    );
+  });
+}
